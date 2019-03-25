31f0992c5b2a8896f21842e39592a35b4eae1e32
Jeff Tian <jie.tian@unicareer.com>
2019-03-25 17:41:29 +0800

    add more type def

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import RetryAction from './retry'
 import RetryAsyncAction from './retryAsync'
 
+type Handlers = {
+  error: string | RegExp
+  handler: () => void
+}[]
+
 export default class Action {
-  static retry(action, maxRetryCount, handlers, logger = console) {
+  static retry(action: () => void, maxRetryCount: number, handlers: Handlers, logger = console) {
     return RetryAction.retry(action, maxRetryCount, handlers, logger)
   }
 
-  static retryAsync(action, maxRetryCount, handlers, logger = console) {
+  static retryAsync(action: () => Promise<void>, maxRetryCount: number, handlers: Handlers, logger = console) {
     return RetryAsyncAction.retry(action, maxRetryCount, handlers, logger)
   }
 }
